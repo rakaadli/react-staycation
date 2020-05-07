ed3b945b07ebf7d55ec3d163a8db33f05f8bf2f7
rakaadli <34180704+rakaadli@users.noreply.github.com>
2020-05-07 14:34:56 +0700

    commit ke 3 lengkap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 // import logo from "./logo.svg";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { ToastContainer } from "react-toastify";
 import "./App.css";
 import "./assets/scss/style.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
+// import Example from "pages/Example";
+import DetailsPage from "pages/DetailsPage";
+import Checkout from "pages/Checkout";
+import Example from "pages/Example";
 
 function App() {
   return (
@@ -24,8 +30,12 @@ function App() {
     //   </header>
     // </div>
     <Router>
-      <Route path='/' component={LandingPage}></Route>
+      <Route exact path='/' component={LandingPage} />
+      <Route exact path='/properties/:id' component={DetailsPage} />
+      <Route path='/checkout' component={Checkout} />
+      {/* <Route path='/example' component={Example} /> */}
     </Router>
+    //  <ToastContainer></ToastContainer>
   );
 }
 
